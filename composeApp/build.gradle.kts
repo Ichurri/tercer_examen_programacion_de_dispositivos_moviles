9a37e00b71a2b67f962fe1a07ac2156bdeabc6f3
ichurri <ichurri@debian>
2026-04-22 18:45:36 -0400

    feat(Firebase): Add Firebase dependencies

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -35,6 +35,9 @@ kotlin {
             implementation(libs.androidx.work.runtime)
             implementation(libs.androidx.lifecycle.process)
             implementation(libs.kotlin.serialization.json)
+            implementation(platform(libs.firebase.bom))
+            implementation(libs.firebase.database)
+            implementation(libs.firebase.common)
         }
         commonMain.dependencies {
             implementation(libs.compose.runtime)
