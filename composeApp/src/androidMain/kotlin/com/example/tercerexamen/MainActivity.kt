47e160b2febd3dc6932d1bcac15d038f3a9a9399
ichurri <ichurri@debian>
2026-04-22 18:36:52 -0400

    feat(T4): Initialize AppEventTracker in MainActivity

--- composeApp/src/androidMain/kotlin/com/example/tercerexamen/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/example/tercerexamen/MainActivity.kt
@@ -6,11 +6,15 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.tercerexamen.service.AppEventTracker
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
+
+        // Inicializar rastreador de eventos
+        AppEventTracker.getInstance(this)
 
         setContent {
             App()
